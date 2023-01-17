73ff5f27d31a1654bde92bd13c89af9c29150374
Ashutosh gautam <ashutosh251848@gmail.com>
2023-01-17 15:24:49 +0530

    Added solution - LeetHub

---
+++ Smallest sum contiguous subarray - GFG/smallest-sum-contiguous-subarray.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& v){
+      int ans = INT_MAX, n = v.size(), cur = 0;
+      for(int i = 0; i < n; i++)
+      {
+          cur += v[i];
+          ans = min(ans,cur);
+          
+          if(cur > 0) cur = 0;
+      }
+      return ans;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
