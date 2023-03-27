2ff1f06886d060e45c154cf57e159f3ad98e53d9
Ashutosh gautam <ashutosh251848@gmail.com>
2023-03-27 13:48:58 +0530

    Added solution - LeetHub

--- Divide in Incremental Groups - GFG/divide-in-incremental-groups.cpp
+++ Divide in Incremental Groups - GFG/divide-in-incremental-groups.cpp
@@ -11,25 +11,21 @@ using namespace std;
 class Solution {
   public:
     int dp[101][101][101];
-    int func(int prev, int done, int rem, int &n, int &k)
+    int func(int prev, int g, int rem, int &N, int &K)
     {
-        if(rem == 0 && done == k) return 1;
-        if(rem == 0 || done == k) return 0;
-    
-        if(dp[prev][done][rem] != -1) return dp[prev][done][rem];
+        if(g == K && rem == 0) return 1;
+        if(g == K || rem == 0) return 0;
+        
+        if(dp[prev][g][rem] != -1) return dp[prev][g][rem];
         int ans = 0;
-    
         for(int ele = prev; ele <= rem; ele++)
-        {
-            ans += func(ele, done + 1, rem - ele, n, k);
-        }
-        return dp[prev][done][rem] = ans;
+            ans += func(ele, g+1, rem - ele, N, K);
+            
+        return dp[prev][g][rem] = ans;
     }
-    int countWaystoDivide(int N, int K) 
-    {
+    int countWaystoDivide(int N, int K) {
         memset(dp, -1, sizeof(dp));
-        int ans = func(1, 0, N, N, K);
-        return ans;
+        return func(1,0,N, N, K);
     }
 };
 
